01f53aceaad71b06050b7c24b224a3bfa2a53162
Sohail Nasir <sohailnasir74business@gmail.com>
2022-12-11 00:07:05 +0500

    todays end

--- src/Redux/feature/auth/services/authServices.js
+++ src/Redux/feature/auth/services/authServices.js
@@ -1,18 +1,24 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-const url = process.env.BACK_END_URL
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
+console.log(process.env);
+console.log(BACKEND_URL);
 
-
-const register = async ({userData})=>{
+export const emailValidation = async (email)=>{
+    let pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    return pattern.test(email);
+}
+export const registerUser = async (userData)=>{
+    
     try {
-        const response = await axios.post(`${url}api/users/register`, userData, {withCredentials: true} )
+        const response = await axios.post(`${BACKEND_URL}/api/users/register`, userData, {withCredentials: true} )
         if(response.statusText==="OK"){
                 toast.success("User Registred Successfully")
         }
         return response.data
         
     } catch (error) {
-        const message = (error.message && error.response.message && error.response.data && error.response.data.message) || error.message || error.toStringyfy()
+        const message = error.response.data.message  || error.message || error.toStringyfy()
         toast.error(message)
     }
     
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,9 +1,64 @@
 import styles from '../auth/auth.module.scss'
 import Card from '../../components/cards/Cards'
 import {TiUserAddOutline} from "react-icons/ti"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { emailValidation, registerUser } from '../../Redux/feature/auth/services/authServices'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { useDispatch } from 'react-redux'
+import { SET_LOGIN, SET_NAME } from '../../Redux/feature/auth/authSlice'
+
+
+const initialData = {
+  name: "",
+  email: "",
+  password: "",
+  password2: "",
+}
 
 const Register = () => {
+  const despatch = useDispatch()
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState(initialData)
+  const [isLoading, setIsLoading] = useState(false)
+  const {name, email, password, password2} = formData
+  
+  const handleInPutChange = (e)=>{
+    const {name, value} = e.target
+    setFormData({...formData, [name]: value})
+
+  }
+  const register = async (e)=>{
+    e.preventDefault()
+    if(!name || !email || !password || !password2){
+      toast.error("All fields must be entered")
+    }
+    if(password !== password2){
+      toast.error("Please type correct password")
+    }
+    if(password.length<6){
+      toast.error("Passowrd must be at least 6 digits")
+    }
+    if(!emailValidation){
+      toast.error("Email is not valid")
+    }
+    const userData = {name, email, password}
+    setIsLoading(true)
+    try {
+      const data = await registerUser(userData)
+      await despatch(SET_LOGIN(true))
+      await despatch(SET_NAME(data.name))
+      navigate("/dashboard")
+      setIsLoading(false)
+      
+    } catch (error) {
+      setIsLoading(false)
+      console.log(error.message);
+    }
+    
+
+  }
   return (
     <div className={`container ${styles.auth}`}>
       <Card>
@@ -13,11 +68,11 @@ const Register = () => {
                 
             </div>
             <h2>Register</h2>
-            <form>
-            <input type="text" placeholder='Name' required name='Name'/>
-              <input type="text" placeholder='Email' required name='Email'/>
-              <input type="password"  placeholder='Password' required name='password'/>
-              <input type="password"  placeholder='Confirm Password' required name='password'/>
+            <form onSubmit={register}>
+            <input type="text" placeholder='Name' required name='name' value={name} onChange={handleInPutChange}/>
+              <input type="text" placeholder='Email' required name='email' value={email} onChange={handleInPutChange}/>
+              <input type="password"  placeholder='Password' required name='password' value={password} onChange={handleInPutChange}/>
+              <input type="password"  placeholder='Confirm Password' required name='password2' value={password2} onChange={handleInPutChange}/>
               <button type='submit' className='--btn --btn-primary --btn-block'>Register</button>
             </form>
             <span className={styles.register}>
